f4f770279c54a56def19fd1436f214eda4b15cd2
skrokmateusz <skrok.mat@gmail.com>
2023-08-31 14:19:45 +0200

    try with images opacity

--- js/script.js
+++ js/script.js
@@ -1,70 +1,76 @@
-const burgerButton = document.querySelector('.burger-btn')
-const burgerNav = document.querySelector('.burger-nav')
-const burgerIcon = document.querySelector('.burger-btn__bars')
-const imgElement = document.querySelector('.wrapper')
+const burgerButton = document.querySelector('.burger-btn');
+const burgerNav = document.querySelector('.burger-nav');
+const burgerIcon = document.querySelector('.burger-btn__bars');
+const imgElement = document.querySelector('.wrapper');
 
 const navigationHandler = () => {
-	burgerNav.classList.toggle('burger-nav--active')
-	burgerIcon.classList.toggle('burger-btn__bars--active')
-}
+	burgerNav.classList.toggle('burger-nav--active');
+	burgerIcon.classList.toggle('burger-btn__bars--active');
+};
 
 // const images = ["url('../img/358.JPG')", '../img/IMG_1674.jpg', '../img/IMG_1866.JPG', '../img/IMG_6807.JPG', '../img/IMG_7110.JPG']
 
-
-let index = 1
-let isInitial = true
-let imgChanger = null
+let index = 1;
+let isInitial = true;
+let imgChanger = null;
 
 const initialChangeImageHandler = () => {
 	if (isInitial) {
+		index > 4 ? (index = 1) : index++;
+		const currentImage = document.querySelector(`.image${index}`);
+		currentImage.classList.add('visible');
+		if (index === 1) {
+			const prevImage = document.querySelector(`.image5`);
+			prevImage.classList.remove('visible')
+		} else {
+			const prevImage = document.querySelector(`.image${index - 1}`)
+			prevImage.classList.remove('visible')
+		}
 		
-		imgElement.classList.remove(`image${index}`)
-		index > 4 ? (index = 1) : index++
-		imgElement.classList.add(`image${index}`)
 	}
-}
-const initialImgChanger = setInterval(initialChangeImageHandler, 3000)
+};
+const initialImgChanger = setInterval(initialChangeImageHandler, 3000);
 
-const lostHoverOnLiItem = e => {
-	const deleteClass = Array.from(imgElement.classList)
-	const startIndex = deleteClass[1].slice(-1)
-	const changeImageHandler = () => {
-		imgElement.classList.remove(`image${startIndex}`)
-		imgElement.classList.remove(`image${index}`)
-		index > 4 ? (index = 1) : index++
-		imgElement.classList.add(`image${index}`)
-	}
-	imgChanger = setInterval(changeImageHandler, 3000)
-	return imgChanger
-}
+// const lostHoverOnLiItem = e => {
+// 	const deleteClass = Array.from(imgElement.classList);
+// 	const startIndex = deleteClass[1].slice(-1);
+// 	const changeImageHandler = () => {
+// 		imgElement.classList.remove(`image${startIndex}`);
+// 		imgElement.classList.remove(`image${index}`);
+// 		index > 4 ? (index = 1) : index++;
+// 		imgElement.classList.add(`image${index}`);
+// 	};
+// 	imgChanger = setInterval(changeImageHandler, 3000);
+// 	return imgChanger;
+// };
 
 const hoverOnLiItem = e => {
-	const burgerIcon = e.target.closest('div').className
+	const burgerIcon = e.target.closest('div').className;
 	if (burgerIcon !== 'nav__container') {
-		return
+		return;
 	}
-	const deleteClass = Array.from(imgElement.classList)
-	imgElement.classList.remove(deleteClass[1])
+	const deleteClass = Array.from(imgElement.classList);
+	imgElement.classList.remove(deleteClass[1]);
 	if (isInitial) {
-		clearInterval(initialImgChanger)
+		clearInterval(initialImgChanger);
 	}
 	if (imgChanger) {
-		clearInterval(imgChanger)
-		imgChanger = null
+		clearInterval(imgChanger);
+		imgChanger = null;
 	}
-	const liItem = e.target.closest('li')
-	imgElement.classList.add(`image${liItem.id}`)
-	isInitial = false
-}
+	const liItem = e.target.closest('li');
+	imgElement.classList.add(`image${liItem.id}`);
+	isInitial = false;
+};
 
-const pageLoaderHandler = () => {
-	const spinnerContainer = document.querySelector('.spinner-container')
-	const spinner = document.querySelector('.spinner')
-	spinnerContainer.classList.add('spinner-container-hidden')
-	spinner.classList.add('spinner-hidden')
-}
+// const pageLoaderHandler = () => {
+// 	const spinnerContainer = document.querySelector('.spinner-container');
+// 	const spinner = document.querySelector('.spinner');
+// 	spinnerContainer.classList.add('spinner-container-hidden');
+// 	spinner.classList.add('spinner-hidden');
+// };
 
-window.addEventListener('load', pageLoaderHandler)
-burgerButton.addEventListener('click', navigationHandler)
-document.body.addEventListener('mouseover', hoverOnLiItem)
-document.body.addEventListener('mouseleave', lostHoverOnLiItem)
+// window.addEventListener('load', pageLoaderHandler);
+burgerButton.addEventListener('click', navigationHandler);
+// document.body.addEventListener('mouseover', hoverOnLiItem);
+// document.body.addEventListener('mouseleave', lostHoverOnLiItem);
