55103630658dac5c21775bb41fe6d53755192617
skrokmateusz <skrok.mat@gmail.com>
2023-08-31 18:21:39 +0200

    correct display of images

--- js/script.js
+++ js/script.js
@@ -3,16 +3,18 @@ const burgerNav = document.querySelector('.burger-nav');
 const burgerIcon = document.querySelector('.burger-btn__bars');
 const imgElement = document.querySelector('.wrapper');
 
+const imageArray = document.querySelectorAll('.image');
+
 const navigationHandler = () => {
 	burgerNav.classList.toggle('burger-nav--active');
 	burgerIcon.classList.toggle('burger-btn__bars--active');
 };
 
-// const images = ["url('../img/358.JPG')", '../img/IMG_1674.jpg', '../img/IMG_1866.JPG', '../img/IMG_6807.JPG', '../img/IMG_7110.JPG']
-
 let index = 1;
+document.querySelector('.image1').classList.add('visible');
 let isInitial = true;
 let imgChanger = null;
+let indexOfCurrentImage;
 
 const initialChangeImageHandler = () => {
 	if (isInitial) {
@@ -21,56 +23,66 @@ const initialChangeImageHandler = () => {
 		currentImage.classList.add('visible');
 		if (index === 1) {
 			const prevImage = document.querySelector(`.image5`);
-			prevImage.classList.remove('visible')
+			prevImage.classList.remove('visible');
 		} else {
-			const prevImage = document.querySelector(`.image${index - 1}`)
-			prevImage.classList.remove('visible')
+			const prevImage = document.querySelector(`.image${index - 1}`);
+			prevImage.classList.remove('visible');
 		}
-		
 	}
 };
 const initialImgChanger = setInterval(initialChangeImageHandler, 3000);
-
-// const lostHoverOnLiItem = e => {
-// 	const deleteClass = Array.from(imgElement.classList);
-// 	const startIndex = deleteClass[1].slice(-1);
-// 	const changeImageHandler = () => {
-// 		imgElement.classList.remove(`image${startIndex}`);
-// 		imgElement.classList.remove(`image${index}`);
-// 		index > 4 ? (index = 1) : index++;
-// 		imgElement.classList.add(`image${index}`);
-// 	};
-// 	imgChanger = setInterval(changeImageHandler, 3000);
-// 	return imgChanger;
-// };
 
 const hoverOnLiItem = e => {
 	const burgerIcon = e.target.closest('div').className;
 	if (burgerIcon !== 'nav__container') {
 		return;
 	}
-	const deleteClass = Array.from(imgElement.classList);
-	imgElement.classList.remove(deleteClass[1]);
 	if (isInitial) {
 		clearInterval(initialImgChanger);
 	}
 	if (imgChanger) {
 		clearInterval(imgChanger);
-		imgChanger = null;
 	}
-	const liItem = e.target.closest('li');
-	imgElement.classList.add(`image${liItem.id}`);
+
+	imageArray.forEach(item => item.classList.remove('visible'));
+	indexOfCurrentImage = e.target.closest('li').id;
+	const currentImage = document.querySelector(`.image${indexOfCurrentImage}`);
+	currentImage.classList.add('visible');
 	isInitial = false;
 };
 
-// const pageLoaderHandler = () => {
-// 	const spinnerContainer = document.querySelector('.spinner-container');
-// 	const spinner = document.querySelector('.spinner');
-// 	spinnerContainer.classList.add('spinner-container-hidden');
-// 	spinner.classList.add('spinner-hidden');
-// };
+const lostHoverOnLiItem = e => {
+	const burgerIcon = e.target.closest('div');
+	if (e.target) {
+		const changeImageHandler = () => {
+			const imageOnLostHover = document.querySelector(`.image${indexOfCurrentImage}`);
+			console.log(imageOnLostHover);
+			imageOnLostHover.classList.remove('visible');
+			index = indexOfCurrentImage;
+			console.log(index, 'index');
+			indexOfCurrentImage > 4 ? (indexOfCurrentImage = 1) : indexOfCurrentImage++;
+			const currentImage = document.querySelector(`.image${indexOfCurrentImage}`);
+			currentImage.classList.add('visible');
+			if (indexOfCurrentImage === 1) {
+				const prevImage = document.querySelector(`.image5`);
+				prevImage.classList.remove('visible');
+			} else {
+				const prevImage = document.querySelector(`.image${indexOfCurrentImage - 1}`);
+				prevImage.classList.remove('visible');
+			}
+		};
+		imgChanger = setInterval(changeImageHandler, 3000);
+	}
+};
 
-// window.addEventListener('load', pageLoaderHandler);
+const pageLoaderHandler = () => {
+	const spinnerContainer = document.querySelector('.spinner-container');
+	const spinner = document.querySelector('.spinner');
+	spinnerContainer.classList.add('spinner-container-hidden');
+	spinner.classList.add('spinner-hidden');
+};
+
+window.addEventListener('load', pageLoaderHandler);
 burgerButton.addEventListener('click', navigationHandler);
-// document.body.addEventListener('mouseover', hoverOnLiItem);
-// document.body.addEventListener('mouseleave', lostHoverOnLiItem);
+document.body.addEventListener('mouseover', hoverOnLiItem);
+document.body.addEventListener('mouseleave', lostHoverOnLiItem);
