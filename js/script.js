5ed49a4708ccc0f1889afd6b12e8014b770a4b87
skrokmateusz <skrok.mat@gmail.com>
2023-08-31 12:18:01 +0200

    try with images

--- js/script.js
+++ js/script.js
@@ -9,19 +9,14 @@ const navigationHandler = () => {
 }
 
 // const images = ["url('../img/358.JPG')", '../img/IMG_1674.jpg', '../img/IMG_1866.JPG', '../img/IMG_6807.JPG', '../img/IMG_7110.JPG']
+console.log('loading ...');
 
-imgElement.classList.add(`image2`)
-imgElement.classList.remove(`image2`)
-imgElement.classList.add(`image3`)
-imgElement.classList.remove(`image3`)
-imgElement.classList.add(`image4`)
-imgElement.classList.remove(`image4`)
-imgElement.classList.add(`image5`)
-imgElement.classList.remove(`image5`)
+imgElement.classList.remove('image2')
+imgElement.classList.remove('image3')
+imgElement.classList.remove('image4')
+imgElement.classList.remove('image5')
 
-
-
-imgElement.classList.add(`image1`)
+// imgElement.classList.add(`image1`)
 let index = 1
 let isInitial = true
 let imgChanger = null
