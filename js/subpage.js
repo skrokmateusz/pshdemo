b80b78e7da860bbc24b2e1844e8054f78ae2c98a
skrokmateusz <skrok.mat@gmail.com>
2023-09-23 19:58:51 +0200

    added contact subpage

--- js/subpage.js
+++ js/subpage.js
@@ -1,20 +1,4 @@
-const observer = new IntersectionObserver(
-	entries => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				console.log(entry.target.id)
-				entry.target.classList.add('show')
-				entry.target.classList.remove('hidden')
-			} else {
-				entry.target.classList.add('hidden')
-				// entry.target.classList.remove('show')
-			}
-		})
-	},
-	{
-		threshold: 0.1,
-	}
-)
+
 
 const pageLoaderHandler = () => {
 	const spinnerContainer = document.querySelector('.spinner-container')
@@ -25,5 +9,4 @@ const pageLoaderHandler = () => {
 
 window.addEventListener('load', pageLoaderHandler)
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach(el => observer.observe(el))
+
